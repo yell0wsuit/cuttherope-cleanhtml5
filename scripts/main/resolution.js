66f99bc5ce6c6b6da352e1fdd7d8cd2b909cf9a1
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-04 16:41:05 +0700

    remove unused variable in resolution.js

--- scripts/main/resolution.js
+++ scripts/main/resolution.js
@@ -5,8 +5,7 @@ define("resolution", [
     "config/resolutions/768x432",
 ], function (settings, scaleResolution, res480, res768) {
     // decide which resolution to target
-    var $w = $(window),
-        resolution;
+    var resolution;
 
     // Use 480px (for testing in desktop browser)
     resolution = res480;
