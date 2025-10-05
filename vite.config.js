d7f46c3c5726de0e5ade53480c6d6948a65cdec3
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-05 22:30:05 +0700

    add workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+    base: "/cuttherope-cleanhtml5/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
