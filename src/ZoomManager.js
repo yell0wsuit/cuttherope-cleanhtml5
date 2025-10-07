5c856229233b81dbbeb17cd3c666b438be8f36f9
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-07 22:57:11 +0700

    Enable auto-resizing in ZoomManager.domReady

--- src/ZoomManager.js
+++ src/ZoomManager.js
@@ -1,12 +1,15 @@
 import ZoomManager from "@/config/resolutions/ZoomManager";
 import resolution from "@/resolution";
+
 ZoomManager.domReady = function () {
-    // no scaling, just center the game
     this.setElementId("gameContainer");
 
     this.nativeWidth = resolution.UI_WIDTH;
     this.nativeHeight = resolution.UI_HEIGHT;
-    this.resize(true /* skipZoom */);
+    // this.resize(true /* skipZoom */);
+
+    // Enable auto-resizing instead of skipping zoom
+    this.autoResize();
 };
 
 export default ZoomManager;
