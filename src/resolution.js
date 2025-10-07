b41d2b18c2cc91f0545b93827d027fa8b1eb0d63
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-07 21:56:57 +0700

    Update resolution.js

--- src/resolution.js
+++ src/resolution.js
@@ -3,11 +3,12 @@ import scaleResolution from "@/config/resolutions/scale";
 import res480 from "@/config/resolutions/480x270";
 import res768 from "@/config/resolutions/768x432";
 import res1024 from "@/config/resolutions/1024x576";
+import res1920 from "@/config/resolutions/1920x1080";
 
 // decide which resolution to target
 let resolution;
 
-resolution = res1024;
+resolution = res1920;
 
 scaleResolution(resolution);
 resolution.isHD = true;
