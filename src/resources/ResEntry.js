e319bf1fa572c4be84ab49182d2fc50d05d3341c
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-16 20:42:02 +0700

    update to use Class

--- src/resources/ResEntry.js
+++ src/resources/ResEntry.js
@@ -3,25 +3,27 @@
  * @param path {string} location of the file
  * @param type {ResourceType} resource type (IMAGE, SOUND, etc)
  */
-const ResEntry = function (path, type, options = {}) {
-    this.path = path;
-    this.type = type;
+class ResEntry {
+    constructor(path, type, options = {}) {
+        this.path = path;
+        this.type = type;
 
-    if (options.atlasPath) {
-        this.atlasPath = options.atlasPath;
-    }
+        if (options.atlasPath) {
+            this.atlasPath = options.atlasPath;
+        }
 
-    if (options.atlasFormat) {
-        this.atlasFormat = options.atlasFormat;
-    }
+        if (options.atlasFormat) {
+            this.atlasFormat = options.atlasFormat;
+        }
 
-    if (options.frameOrder) {
-        this.frameOrder = options.frameOrder.slice();
-    }
+        if (options.frameOrder) {
+            this.frameOrder = options.frameOrder.slice();
+        }
 
-    if (options.offsetNormalization) {
-        this.offsetNormalization = options.offsetNormalization;
+        if (options.offsetNormalization) {
+            this.offsetNormalization = options.offsetNormalization;
+        }
     }
-};
+}
 
 export default ResEntry;
