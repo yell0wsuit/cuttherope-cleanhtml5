3c574fb289f99f69869dd8ab4c6d5e307baa3378
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-18 19:08:50 +0700

    Refactor to use object literals and functions for modules
    Refactored Easing and QueryStrings modules to use object literals and standalone functions instead of function constructors. Simplified Class.js by removing IIFE and directly defining the Class and extend logic.

--- src/ui/QueryStrings.js
+++ src/ui/QueryStrings.js
@@ -1,53 +1,54 @@
 // putting all query strings in a central location so that we can easily add/remove for ship
-const QueryStrings = new (function () {
-    // parse the query strings into a dictionary
-    function getQueryStrings() {
-        const assoc = {},
-            queryString = location.search.substring(1) || "",
-            keyValues = queryString.split("&"),
-            decode = function (s) {
-                return decodeURIComponent(s.replace(/\+/g, " "));
-            };
-        let i, len, kv;
 
-        for (i = 0, len = keyValues.length; i < len; i++) {
-            kv = keyValues[i].split("=");
-            if (kv.length > 1) {
-                assoc[decode(kv[0])] = decode(kv[1]);
-            }
+// parse the query strings into a dictionary
+function getQueryStrings() {
+    const assoc = {},
+        queryString = location.search.substring(1) || "",
+        keyValues = queryString.split("&"),
+        decode = function (s) {
+            return decodeURIComponent(s.replace(/\+/g, " "));
+        };
+    let i, len, kv;
+
+    for (i = 0, len = keyValues.length; i < len; i++) {
+        kv = keyValues[i].split("=");
+        if (kv.length > 1) {
+            assoc[decode(kv[0])] = decode(kv[1]);
         }
-        return assoc;
     }
+    return assoc;
+}
 
-    const qs = getQueryStrings();
+const qs = getQueryStrings();
 
-    // case insensitive lookup
-    const urlContains = function (val) {
-        const url = window.location.href.toLowerCase();
-        return url.indexOf(val.toLowerCase()) >= 0;
-    };
+// case insensitive lookup
+const urlContains = function (val) {
+    const url = window.location.href.toLowerCase();
+    return url.indexOf(val.toLowerCase()) >= 0;
+};
 
-    // debug querystrings
-    /*if (false || false) {
-        this.box = qs["box"] == null ? null : parseInt(qs["box"], 10);
-        this.level = qs["level"] == null ? null : parseInt(qs["level"], 10);
-        this.minFps = qs["minFps"] == null ? null : parseInt(qs["minFps"], 10);
-        this.unlockAllBoxes =
-            (this.box != null && this.level != null) || urlContains("unlockAllBoxes=true");
-        this.forcePinnedBox = this.unlockAllBoxes || urlContains("enablePinnedBox=true");
-        this.createScoresForBox =
-            qs["createScoresForBox"] == null ? null : parseInt(qs["createScoresForBox"], 10);
-    }*/
+// debug querystrings
+/*if (false || false) {
+    QueryStrings.box = qs["box"] == null ? null : parseInt(qs["box"], 10);
+    QueryStrings.level = qs["level"] == null ? null : parseInt(qs["level"], 10);
+    QueryStrings.minFps = qs["minFps"] == null ? null : parseInt(qs["minFps"], 10);
+    QueryStrings.unlockAllBoxes =
+        (QueryStrings.box != null && QueryStrings.level != null) || urlContains("unlockAllBoxes=true");
+    QueryStrings.forcePinnedBox = QueryStrings.unlockAllBoxes || urlContains("enablePinnedBox=true");
+    QueryStrings.createScoresForBox =
+        qs["createScoresForBox"] == null ? null : parseInt(qs["createScoresForBox"], 10);
+}*/
 
-    // these are ok to leave in for ship
-    this.lang = qs["lang"];
-    this.showBoxBackgrounds = urlContains("boxBackgrounds=true");
-    this.showFrameRate = urlContains("showFrameRate=true");
-    this.forceHtml5Audio = urlContains("html5audio=true");
+// these are ok to leave in for ship
+const QueryStrings = {
+    lang: qs["lang"],
+    showBoxBackgrounds: urlContains("boxBackgrounds=true"),
+    showFrameRate: urlContains("showFrameRate=true"),
+    forceHtml5Audio: urlContains("html5audio=true"),
+};
 
-    // for testing
-    //this.unlockAllBoxes = true;
-    //this.showFrameRate = true;
-})();
+// for testing
+//QueryStrings.unlockAllBoxes = true;
+//QueryStrings.showFrameRate = true;
 
 export default QueryStrings;
