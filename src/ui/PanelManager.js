64f81d00c515a3abdd5e1f9c9044eb46415ed27e
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-18 20:46:48 +0700

    Refactor managers to use ES6 classes
    Converted BoxManager, PanelManager, ScoreManager, and SocialHelper from IIFE singletons to ES6 class-based singletons for improved readability and maintainability. Updated event subscriptions and internal method calls to use class instance context.

--- src/ui/PanelManager.js
+++ src/ui/PanelManager.js
@@ -8,282 +8,291 @@ import platform from "@/platform";
 import Easing from "@/ui/Easing";
 import PubSub from "@/utils/PubSub";
 import edition from "@/edition";
-const PanelManager = new (function () {
-    const _this = this,
-        panels = [];
+class PanelManagerClass {
+    constructor() {
+        this.panels = [];
+        this.onShowPanel = null;
+        this.currentPanelId = PanelId.MENU;
 
-    const getElementById = (id) => (id ? document.getElementById(id) : null);
-    const showElementById = (id) => {
-        const el = getElementById(id);
-        if (el) el.style.display = "block";
-    };
-    const hideElementById = (id) => {
-        const el = getElementById(id);
-        if (el) el.style.display = "none";
-    };
-    const setElementOpacity = (el, value) => {
-        if (el) el.style.opacity = String(value);
-    };
-    const setElementDisplay = (el, value) => {
-        if (el) el.style.display = value;
-    };
+        // Fade parameters
+        this.fadeInDur = 100;
+        this.fadePause = 50;
+        this.fadeOutDur = 100;
+        this.fadeTo = 1.0;
+        this.fadeToBlack = null;
+        this.isFading = false;
 
-    this.onShowPanel = null;
+        // Shadow parameters
+        this.shadowIsRotating = false;
+        this.shadowAngle = 15.0;
+        this.shadowCanvas = null;
+        this.shadowImage = null;
+        this.shadowOpacity = 1.0;
+        this.shadowIsVisible = false;
+        this.shadowSpeedup = edition.shadowSpeedup || 1;
+        this.shadowPanelElement = null;
 
-    this.domReady = function () {
-        fadeToBlack = document.getElementById("fadeToBlack");
-        shadowCanvas = document.getElementById("shadowCanvas");
-        shadowPanelElement = document.getElementById("shadowPanel");
+        // create our panels
+        this.panels.push(new Panel(PanelId.MENU, "menuPanel", "startBackground", true));
+        this.panels.push(BoxPanel);
+        this.panels.push(LevelPanel);
 
-        shadowCanvas = document.getElementById("shadowCanvas");
-        shadowCanvas.width = resolution.uiScaledNumber(1024);
-        shadowCanvas.height = resolution.uiScaledNumber(576);
-    };
+        // the game panel re-uses the panel doors in the levelBackground (actually in foreground)
+        this.panels.push(new Panel(PanelId.GAME, null, "levelBackground", false));
+        this.panels.push(new Panel(PanelId.GAMEMENU, null, null, false));
+        this.panels.push(new Panel(PanelId.LEVELCOMPLETE, null, null, false));
+        this.panels.push(new Panel(PanelId.GAMECOMPLETE, "gameCompletePanel", "menuBackground", true));
+        this.panels.push(new Panel(PanelId.OPTIONS, "optionsPanel", "menuBackground", true));
+        this.panels.push(new Panel(PanelId.CREDITS, null, null, false));
+        this.panels.push(new Panel(PanelId.LEADERBOARDS, "leaderboardPanel", "menuBackground", true));
+        this.panels.push(new Panel(PanelId.ACHIEVEMENTS, "achievementsPanel", "menuBackground", true));
+        this.panels.push(PasswordPanel);
+    }
 
-    this.appReady = function (onInitializePanel) {
+    domReady() {
+        this.fadeToBlack = document.getElementById("fadeToBlack");
+        this.shadowCanvas = document.getElementById("shadowCanvas");
+        this.shadowPanelElement = document.getElementById("shadowPanel");
+
+        this.shadowCanvas = document.getElementById("shadowCanvas");
+        this.shadowCanvas.width = resolution.uiScaledNumber(1024);
+        this.shadowCanvas.height = resolution.uiScaledNumber(576);
+    }
+
+    appReady(onInitializePanel) {
         // we have to wait until the game is ready to run before initializing
         // panels because we need the fonts to be loaded
 
-        shadowImage = new Image();
-        shadowImage.src = platform.uiImageBaseUrl + "shadow.png";
+        this.shadowImage = new Image();
+        this.shadowImage.src = platform.uiImageBaseUrl + "shadow.png";
 
         // initialize each of the panels
         if (onInitializePanel) {
-            for (let i = 0, len = panels.length; i < len; i++) {
-                onInitializePanel(panels[i].id);
+            for (let i = 0, len = this.panels.length; i < len; i++) {
+                onInitializePanel(this.panels[i].id);
             }
         }
-    };
+    }
 
     // get a panel by id
-    const getPanelById = (this.getPanelById = function (panelId) {
-        for (let i = 0; i < panels.length; i++) {
-            if (panels[i].id == panelId) return panels[i];
+    getPanelById(panelId) {
+        for (let i = 0; i < this.panels.length; i++) {
+            if (this.panels[i].id == panelId) return this.panels[i];
         }
         return null;
-    });
-
-    // create our panels
-    panels.push(new Panel(PanelId.MENU, "menuPanel", "startBackground", true));
-    panels.push(BoxPanel);
-    panels.push(LevelPanel);
-
-    // the game panel re-uses the panel doors in the levelBackground (actually in foreground)
-    panels.push(new Panel(PanelId.GAME, null, "levelBackground", false));
-    panels.push(new Panel(PanelId.GAMEMENU, null, null, false));
-    panels.push(new Panel(PanelId.LEVELCOMPLETE, null, null, false));
-    panels.push(new Panel(PanelId.GAMECOMPLETE, "gameCompletePanel", "menuBackground", true));
-    panels.push(new Panel(PanelId.OPTIONS, "optionsPanel", "menuBackground", true));
-    panels.push(new Panel(PanelId.CREDITS, null, null, false));
-    panels.push(new Panel(PanelId.LEADERBOARDS, "leaderboardPanel", "menuBackground", true));
-    panels.push(new Panel(PanelId.ACHIEVEMENTS, "achievementsPanel", "menuBackground", true));
-    panels.push(PasswordPanel);
-
-    this.currentPanelId = PanelId.MENU;
+    }
 
     // show a panel by id
-    this.showPanel = function (panelId, skipFade) {
-        _this.currentPanelId = panelId;
+    showPanel(panelId, skipFade) {
+        this.currentPanelId = panelId;
 
-        const panel = getPanelById(panelId);
+        const panel = this.getPanelById(panelId);
         const skip = skipFade == null ? false : skipFade;
 
         // enable / disable the shadow animation
         if (panel.showShadow) {
-            showShadow();
+            this._showShadow();
         } else {
-            hideShadow();
+            this._hideShadow();
         }
 
         // we always use a timeout, even if we skip the animation, to keep the code clean
-        const timeout = skip ? 0 : fadeInDur + fadePause;
-        setTimeout(function () {
+        const timeout = skip ? 0 : this.fadeInDur + this.fadePause;
+        setTimeout(() => {
             // show the panel
             if (panel.bgDivId) {
-                showElementById(panel.bgDivId);
+                this._showElementById(panel.bgDivId);
             }
             if (panel.panelDivId) {
-                showElementById(panel.panelDivId);
+                this._showElementById(panel.panelDivId);
             }
 
             // hide other panels
-            for (let i = 0; i < panels.length; i++) {
-                const otherPanel = panels[i];
+            for (let i = 0; i < this.panels.length; i++) {
+                const otherPanel = this.panels[i];
 
                 if (otherPanel.panelDivId != null && otherPanel.panelDivId != panel.panelDivId) {
-                    hideElementById(otherPanel.panelDivId);
+                    this._hideElementById(otherPanel.panelDivId);
                 }
 
                 if (otherPanel.bgDivId != null && otherPanel.bgDivId != panel.bgDivId) {
-                    hideElementById(otherPanel.bgDivId);
+                    this._hideElementById(otherPanel.bgDivId);
                 }
             }
 
             // run the "show" handler
-            if (_this.onShowPanel != null) {
-                _this.onShowPanel(panelId);
+            if (this.onShowPanel != null) {
+                this.onShowPanel(panelId);
             }
 
             // fade back in
             if (!skip) {
-                _this.runBlackFadeOut();
+                this.runBlackFadeOut();
             }
         }, timeout);
 
         // start the animation
         if (!skip) {
-            _this.runBlackFadeIn();
+            this.runBlackFadeIn();
         }
-    };
+    }
 
-    // fade parameters
-    const fadeInDur = 100;
-    const fadePause = 50;
-    const fadeOutDur = 100;
-    const fadeTo = 1.0;
-    let fadeToBlack;
-    let isFading = false;
-
-    this.runBlackFadeIn = function (callback) {
+    runBlackFadeIn(callback) {
         const startTime = Date.now();
 
-        if (!fadeToBlack) {
+        if (!this.fadeToBlack) {
             if (callback) callback();
             return;
         }
 
-        isFading = true;
+        this.isFading = true;
 
-        setElementOpacity(fadeToBlack, 0);
-        setElementDisplay(fadeToBlack, "block");
+        this._setElementOpacity(this.fadeToBlack, 0);
+        this._setElementDisplay(this.fadeToBlack, "block");
 
         // our loop
-        function loop() {
+        const loop = () => {
             const now = Date.now(),
                 diff = now - startTime,
-                v = Easing.noEase(diff, 0, fadeTo, fadeInDur);
+                v = Easing.noEase(diff, 0, this.fadeTo, this.fadeInDur);
 
-            setElementOpacity(fadeToBlack, v);
+            this._setElementOpacity(this.fadeToBlack, v);
 
-            if (diff < fadeInDur) {
+            if (diff < this.fadeInDur) {
                 window.requestAnimationFrame(loop);
             } else {
-                setElementOpacity(fadeToBlack, fadeTo);
+                this._setElementOpacity(this.fadeToBlack, this.fadeTo);
                 if (callback != null) callback();
             }
-        }
+        };
 
         window.requestAnimationFrame(loop);
-    };
+    }
 
-    this.runBlackFadeOut = function () {
-        if (!isFading || !fadeToBlack) return;
+    runBlackFadeOut() {
+        if (!this.isFading || !this.fadeToBlack) return;
         const startTime = Date.now();
 
-        function loop() {
+        const loop = () => {
             const now = Date.now(),
                 diff = now - startTime,
-                v = fadeTo - Easing.noEase(diff, 0, fadeTo, fadeInDur);
+                v = this.fadeTo - Easing.noEase(diff, 0, this.fadeTo, this.fadeInDur);
 
-            setElementOpacity(fadeToBlack, v);
+            this._setElementOpacity(this.fadeToBlack, v);
 
-            if (diff < fadeInDur) {
+            if (diff < this.fadeInDur) {
                 window.requestAnimationFrame(loop);
             } else {
-                setElementOpacity(fadeToBlack, 0);
-                setElementDisplay(fadeToBlack, "none");
-                isFading = false;
+                this._setElementOpacity(this.fadeToBlack, 0);
+                this._setElementDisplay(this.fadeToBlack, "none");
+                this.isFading = false;
             }
-        }
+        };
 
         window.requestAnimationFrame(loop);
-    };
+    }
 
-    let shadowIsRotating = false;
-    let shadowAngle = 15.0;
-    let shadowCanvas = null;
-    let shadowImage = null;
-    let shadowOpacity = 1.0;
-    let shadowIsVisible = false;
-    const shadowSpeedup = edition.shadowSpeedup || 1;
-    let shadowPanelElement = null;
+    // Helper methods
+    _getElementById(id) {
+        return id ? document.getElementById(id) : null;
+    }
 
-    const showShadow = function () {
-        if (!shadowIsVisible) {
-            if (shadowCanvas != null) {
-                const ctx = shadowCanvas.getContext("2d");
+    _showElementById(id) {
+        const el = this._getElementById(id);
+        if (el) el.style.display = "block";
+    }
+
+    _hideElementById(id) {
+        const el = this._getElementById(id);
+        if (el) el.style.display = "none";
+    }
+
+    _setElementOpacity(el, value) {
+        if (el) el.style.opacity = String(value);
+    }
+
+    _setElementDisplay(el, value) {
+        if (el) el.style.display = value;
+    }
+
+    _showShadow() {
+        if (!this.shadowIsVisible) {
+            if (this.shadowCanvas != null) {
+                const ctx = this.shadowCanvas.getContext("2d");
                 ctx.save();
                 ctx.setTransform(1, 0, 0, 1, 0, 0);
-                ctx.clearRect(0, 0, shadowCanvas.width, shadowCanvas.height);
+                ctx.clearRect(0, 0, this.shadowCanvas.width, this.shadowCanvas.height);
                 ctx.restore();
             }
 
-            shadowOpacity = 0.0;
-            shadowIsVisible = true;
+            this.shadowOpacity = 0.0;
+            this.shadowIsVisible = true;
 
-            if (shadowPanelElement) {
-                shadowPanelElement.style.display = "block";
+            if (this.shadowPanelElement) {
+                this.shadowPanelElement.style.display = "block";
             }
-            if (!shadowIsRotating) {
-                beginRotateShadow();
+            if (!this.shadowIsRotating) {
+                this._beginRotateShadow();
             }
         }
-    };
+    }
 
-    const hideShadow = function () {
-        shadowIsVisible = false;
-        shadowIsRotating = false;
-        if (shadowPanelElement) {
-            shadowPanelElement.style.display = "none";
+    _hideShadow() {
+        this.shadowIsVisible = false;
+        this.shadowIsRotating = false;
+        if (this.shadowPanelElement) {
+            this.shadowPanelElement.style.display = "none";
         }
-    };
+    }
 
     // starts the shadow animation
-    const beginRotateShadow = function () {
-        if (!shadowCanvas) return;
-        const ctx = shadowCanvas.getContext("2d"),
+    _beginRotateShadow() {
+        if (!this.shadowCanvas) return;
+        const ctx = this.shadowCanvas.getContext("2d"),
             requestAnimationFrame = window["requestAnimationFrame"];
         let lastRotateTime = Date.now();
-        const renderShadow = function () {
-            if (!shadowIsRotating) {
+        const renderShadow = () => {
+            if (!this.shadowIsRotating) {
                 return;
             }
 
             // move .1 radians every 25 msec
             const now = Date.now(),
                 delta = now - lastRotateTime;
-            shadowAngle += ((delta * 0.1) / 25) * shadowSpeedup;
+            this.shadowAngle += ((delta * 0.1) / 25) * this.shadowSpeedup;
             lastRotateTime = now;
 
             // clear the canvas
             ctx.setTransform(1, 0, 0, 1, 0, 0);
-            ctx.clearRect(0, 0, shadowCanvas.width, shadowCanvas.height);
+            ctx.clearRect(0, 0, this.shadowCanvas.width, this.shadowCanvas.height);
 
             // update opacity
-            if (shadowOpacity < 1.0) {
-                shadowOpacity += 0.025;
-                shadowOpacity = Math.min(shadowOpacity, 1.0);
-                ctx.globalAlpha = shadowOpacity;
+            if (this.shadowOpacity < 1.0) {
+                this.shadowOpacity += 0.025;
+                this.shadowOpacity = Math.min(this.shadowOpacity, 1.0);
+                ctx.globalAlpha = this.shadowOpacity;
             }
 
             // rotate the context
             ctx.save();
-            ctx.translate(shadowImage.width * 0.5, shadowImage.height * 0.5);
+            ctx.translate(this.shadowImage.width * 0.5, this.shadowImage.height * 0.5);
             ctx.translate(resolution.uiScaledNumber(-300), resolution.uiScaledNumber(-510));
-            ctx.rotate((shadowAngle * Math.PI) / 180);
-            ctx.translate(-shadowImage.width * 0.5, -shadowImage.height * 0.5);
+            ctx.rotate((this.shadowAngle * Math.PI) / 180);
+            ctx.translate(-this.shadowImage.width * 0.5, -this.shadowImage.height * 0.5);
 
             // draw the image and update the loop
-            ctx.drawImage(shadowImage, 0, 0);
+            ctx.drawImage(this.shadowImage, 0, 0);
             ctx.restore();
 
             requestAnimationFrame(renderShadow);
         };
 
-        shadowIsRotating = true;
+        this.shadowIsRotating = true;
         renderShadow();
-    };
-})();
+    }
+}
+
+const PanelManager = new PanelManagerClass();
 
 PubSub.subscribe(PubSub.ChannelId.BoxesUnlocked, function (isFirstUnlock) {
     const nextPanelId = isFirstUnlock ? PanelId.MENU : PanelId.BOXES;
--- src/ui/SocialHelper.js
+++ src/ui/SocialHelper.js
@@ -4,39 +4,40 @@ import Lang from "@/resources/Lang";
 import MenuStringId from "@/resources/MenuStringId";
 import PubSub from "@/utils/PubSub";
 import analytics from "@/analytics";
-const SocialHelper = new (function () {
-    this.siteUrl = edition.siteUrl;
+class SocialHelperClass {
+    constructor() {
+        this.siteUrl = edition.siteUrl;
 
-    // cuttherope.ie and cuttherope.net
-    this.appId = "278847552173744";
+        // cuttherope.ie and cuttherope.net
+        this.appId = "278847552173744";
 
-    // check for test domain (seperate FB app ids)
-    const host = window.location.host || "";
-    if (host.indexOf("thinkpixellab") >= 0) {
-        // thinkpixellab.com
-        this.appId = "239041062884795";
-    } else if (host.indexOf(".dev") >= 0) {
-        // ctr-net.dev
-        this.appId = "261043477350153";
+        // check for test domain (seperate FB app ids)
+        const host = window.location.host || "";
+        if (host.indexOf("thinkpixellab") >= 0) {
+            // thinkpixellab.com
+            this.appId = "239041062884795";
+        } else if (host.indexOf(".dev") >= 0) {
+            // ctr-net.dev
+            this.appId = "261043477350153";
+        }
+
+        // listen to language changes
+        PubSub.subscribe(PubSub.ChannelId.LanguageChanged, () => {
+            this.siteDescription = Lang.menuText(MenuStringId.SITE_DESC);
+            this.siteName = Lang.menuText(MenuStringId.SITE_TITLE);
+            this.siteActions = [
+                {
+                    name: Lang.menuText(MenuStringId.SITE_ACTION),
+                    link: edition.siteUrl,
+                },
+            ];
+        });
     }
 
-    // listen to language changes
-    const self = this;
-    PubSub.subscribe(PubSub.ChannelId.LanguageChanged, function () {
-        self.siteDescription = Lang.menuText(MenuStringId.SITE_DESC);
-        self.siteName = Lang.menuText(MenuStringId.SITE_TITLE);
-        self.siteActions = [
-            {
-                name: Lang.menuText(MenuStringId.SITE_ACTION),
-                link: edition.siteUrl,
-            },
-        ];
-    });
-
-    this.initFB = function () {
+    initFB() {
         // NOTE: must create settings this way to prevent obfuscation
         const fbInitSettings = {};
-        fbInitSettings["appId"] = self.appId;
+        fbInitSettings["appId"] = this.appId;
         fbInitSettings["status"] = true;
         fbInitSettings["cookie"] = true;
         fbInitSettings["xfbml"] = true;
@@ -49,10 +50,10 @@ const SocialHelper = new (function () {
                 analytics.onFacebookLike();
             }
         });
-    };
+    }
 
     // remember to return true in the callback
-    this.postToFeed = function (caption, description, imageurl, callback) {
+    postToFeed(caption, description, imageurl, callback) {
         // see if the platform has custom sharing
         if (platform.customSharing) {
             PubSub.publish(PubSub.ChannelId.Share, caption, description, imageurl);
@@ -62,26 +63,28 @@ const SocialHelper = new (function () {
             // NOTE: must create settings this way to prevent obfuscation
             const publish = {};
             publish["method"] = "feed";
-            publish["name"] = self.siteName;
+            publish["name"] = this.siteName;
             publish["caption"] = caption;
             publish["description"] = description;
-            publish["link"] = self.siteUrl;
+            publish["link"] = this.siteUrl;
             publish["picture"] = imageurl;
-            publish["actions"] = self.siteActions;
+            publish["actions"] = this.siteActions;
 
             let FB;
             FB.ui(publish, callback);
         }
-    };
+    }
 
-    this.initTwitter = function (twttr) {
+    initTwitter(twttr) {
         // report tweets from users
         twttr["events"]["bind"]("tweet", function (event) {
             if (analytics.onTweet) {
                 analytics.onTweet();
             }
         });
-    };
-})();
+    }
+}
+
+const SocialHelper = new SocialHelperClass();
 
 export default SocialHelper;
