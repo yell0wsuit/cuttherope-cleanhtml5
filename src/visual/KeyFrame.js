abdf8743721869958a268ef82b84e19a89e7f535
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-19 10:26:57 +0700

    Refactor core classes to ES6 class syntax
    Converted Quad2D, Rectangle, Vector, and KeyFrame from constructor functions and prototype methods to ES6 class syntax.

--- src/visual/KeyFrame.js
+++ src/visual/KeyFrame.js
@@ -24,22 +24,76 @@ class KeyFrameValue {
 }
 
 /**
- * KeyFrame constructor
+ * KeyFrame class
  * @param time {number}
  * @param trackType {TrackType}
  * @param transitionType {KeyFrame.TransitionType}
  * @param value {KeyFrameValue}
  */
-function KeyFrame(time, trackType, transitionType, value) {
-    this.timeOffset = time;
-    this.trackType = trackType;
-    this.transitionType = transitionType;
-    this.value = value;
-}
+class KeyFrame {
+    constructor(time, trackType, transitionType, value) {
+        this.timeOffset = time;
+        this.trackType = trackType;
+        this.transitionType = transitionType;
+        this.value = value;
+    }
 
-KeyFrame.prototype.copy = function () {
-    return new KeyFrame(this.timeOffset, this.trackType, this.transitionType, this.value.copy());
-};
+    copy() {
+        return new KeyFrame(this.timeOffset, this.trackType, this.transitionType, this.value.copy());
+    }
+
+    /**
+     * Creates an empty keyframe
+     * @return {KeyFrame}
+     */
+    static newEmpty() {
+        return new KeyFrame(
+            0, // time
+            TrackType.POSITION, // default track type
+            KeyFrame.TransitionType.LINEAR, // default transition type
+            new KeyFrameValue()
+        );
+    }
+
+    static makePos(x, y, transition, time) {
+        const v = new KeyFrameValue();
+        v.pos.x = x;
+        v.pos.y = y;
+        return new KeyFrame(time, TrackType.POSITION, transition, v);
+    }
+
+    static makeScale(x, y, transition, time) {
+        const v = new KeyFrameValue();
+        v.scale.x = x;
+        v.scale.y = y;
+        return new KeyFrame(time, TrackType.SCALE, transition, v);
+    }
+
+    static makeRotation(r, transition, time) {
+        const v = new KeyFrameValue();
+        v.rotationAngle = r;
+        return new KeyFrame(time, TrackType.ROTATION, transition, v);
+    }
+
+    static makeColor(color, transition, time) {
+        const v = new KeyFrameValue();
+        v.color = color;
+        return new KeyFrame(time, TrackType.COLOR, transition, v);
+    }
+
+    static makeAction(actions, time) {
+        const v = new KeyFrameValue();
+        v.actionSet = actions;
+        return new KeyFrame(time, TrackType.ACTION, KeyFrame.TransitionType.LINEAR, v);
+    }
+
+    static makeSingleAction(target, actionName, actionParam, actionSubParam, time) {
+        const v = new KeyFrameValue(),
+            action = Action.create(target, actionName, actionParam, actionSubParam);
+        v.actionSet = [action];
+        return new KeyFrame(time, TrackType.ACTION, KeyFrame.TransitionType.LINEAR, v);
+    }
+}
 
 /**
  * @enum {number}
@@ -49,58 +103,6 @@ KeyFrame.TransitionType = {
     IMMEDIATE: 1,
     EASE_IN: 2,
     EASE_OUT: 3,
-};
-
-/**
- * Creates an empty keyframe
- * @return {KeyFrame}
- */
-KeyFrame.newEmpty = function () {
-    return new KeyFrame(
-        0, // time
-        TrackType.POSITION, // default track type
-        KeyFrame.TransitionType.LINEAR, // default transition type
-        new KeyFrameValue()
-    );
-};
-
-KeyFrame.makePos = function (x, y, transition, time) {
-    const v = new KeyFrameValue();
-    v.pos.x = x;
-    v.pos.y = y;
-    return new KeyFrame(time, TrackType.POSITION, transition, v);
-};
-
-KeyFrame.makeScale = function (x, y, transition, time) {
-    const v = new KeyFrameValue();
-    v.scale.x = x;
-    v.scale.y = y;
-    return new KeyFrame(time, TrackType.SCALE, transition, v);
-};
-
-KeyFrame.makeRotation = function (r, transition, time) {
-    const v = new KeyFrameValue();
-    v.rotationAngle = r;
-    return new KeyFrame(time, TrackType.ROTATION, transition, v);
-};
-
-KeyFrame.makeColor = function (color, transition, time) {
-    const v = new KeyFrameValue();
-    v.color = color;
-    return new KeyFrame(time, TrackType.COLOR, transition, v);
-};
-
-KeyFrame.makeAction = function (actions, time) {
-    const v = new KeyFrameValue();
-    v.actionSet = actions;
-    return new KeyFrame(time, TrackType.ACTION, KeyFrame.TransitionType.LINEAR, v);
-};
-
-KeyFrame.makeSingleAction = function (target, actionName, actionParam, actionSubParam, time) {
-    const v = new KeyFrameValue(),
-        action = Action.create(target, actionName, actionParam, actionSubParam);
-    v.actionSet = [action];
-    return new KeyFrame(time, TrackType.ACTION, KeyFrame.TransitionType.LINEAR, v);
 };
 
 export default KeyFrame;
