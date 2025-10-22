3c01a4fe276f266697b8c9d2bd32874c0663bc92
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-22 15:23:17 +0700

    Refactor Easing and QueryStrings to use object literals
    Converted Easing and QueryStrings from function constructors to object literals with arrow functions for improved readability and maintainability

--- src/ui/QueryStrings.js
+++ src/ui/QueryStrings.js
@@ -1,55 +1,50 @@
 // putting all query strings in a central location so that we can easily add/remove for ship
-const QueryStrings = new (function () {
-    // parse the query strings into a dictionary
-    function getQueryStrings() {
-        const assoc = {},
-            queryString = location.search.substring(1) || "",
-            keyValues = queryString.split("&"),
-            decode = function (s) {
-                return decodeURIComponent(s.replace(/\+/g, " "));
-            };
-        let i, len, kv;
 
-        for (i = 0, len = keyValues.length; i < len; i++) {
-            kv = keyValues[i].split("=");
-            if (kv.length > 1) {
-                assoc[decode(kv[0])] = decode(kv[1]);
-            }
+// parse the query strings into a dictionary
+const getQueryStrings = () => {
+    const assoc = {};
+    const queryString = location.search.substring(1) || "";
+    const keyValues = queryString.split("&");
+    const decode = (s) => decodeURIComponent(s.replace(/\+/g, " "));
+
+    for (let i = 0, len = keyValues.length; i < len; i++) {
+        const kv = keyValues[i].split("=");
+        if (kv.length > 1) {
+            assoc[decode(kv[0])] = decode(kv[1]);
         }
-        return assoc;
     }
+    return assoc;
+};
 
-    const qs = getQueryStrings();
+const qs = getQueryStrings();
 
-    // case insensitive lookup
-    const urlContains = function (val) {
-        const url = window.location.href.toLowerCase();
-        return url.indexOf(val.toLowerCase()) >= 0;
-    };
+// case insensitive lookup
+const urlContains = (val) => {
+    const url = window.location.href.toLowerCase();
+    return url.indexOf(val.toLowerCase()) >= 0;
+};
 
-    // debug querystrings
-    /*if (false || false) {
-        this.box = qs["box"] == null ? null : parseInt(qs["box"], 10);
-        this.level = qs["level"] == null ? null : parseInt(qs["level"], 10);
-        this.minFps = qs["minFps"] == null ? null : parseInt(qs["minFps"], 10);
-        this.unlockAllBoxes =
-            (this.box != null && this.level != null) || urlContains("unlockAllBoxes=true");
-        this.forcePinnedBox = this.unlockAllBoxes || urlContains("enablePinnedBox=true");
-        this.createScoresForBox =
-            qs["createScoresForBox"] == null ? null : parseInt(qs["createScoresForBox"], 10);
-    }*/
+// debug querystrings
+/*if (false || false) {
+    QueryStrings.box = qs["box"] == null ? null : parseInt(qs["box"], 10);
+    QueryStrings.level = qs["level"] == null ? null : parseInt(qs["level"], 10);
+    QueryStrings.minFps = qs["minFps"] == null ? null : parseInt(qs["minFps"], 10);
+    QueryStrings.unlockAllBoxes =
+        (QueryStrings.box != null && QueryStrings.level != null) || urlContains("unlockAllBoxes=true");
+    QueryStrings.forcePinnedBox = QueryStrings.unlockAllBoxes || urlContains("enablePinnedBox=true");
+    QueryStrings.createScoresForBox =
+        qs["createScoresForBox"] == null ? null : parseInt(qs["createScoresForBox"], 10);
+}*/
 
+const QueryStrings = {
     // these are ok to leave in for ship
-    this.lang = qs["lang"];
-    this.showBoxBackgrounds = urlContains("boxBackgrounds=true");
-    this.showFrameRate = urlContains("showFrameRate=true");
-    this.forceHtml5Audio = urlContains("html5audio=true");
+    lang: qs["lang"],
+    showBoxBackgrounds: urlContains("boxBackgrounds=true"),
+    showFrameRate: import.meta.env.DEV || urlContains("showFrameRate=true"),
+    forceHtml5Audio: urlContains("html5audio=true"),
 
     // for testing
-    if (import.meta.env.DEV) {
-        this.unlockAllBoxes = true;
-        this.showFrameRate = true;
-    }
-})();
+    unlockAllBoxes: import.meta.env.DEV || undefined,
+};
 
 export default QueryStrings;
