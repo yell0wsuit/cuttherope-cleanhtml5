17a250ffecf4c0dba032214272adf1c68f21cdd0
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-22 09:17:24 +0700

    Update RGBAColor.js

--- src/core/RGBAColor.js
+++ src/core/RGBAColor.js
@@ -6,63 +6,59 @@
  * @param b {number} blue
  * @param a {number} alpha
  */
-function RGBAColor(r, g, b, a) {
-    /** type {number} */
-    this.r = r;
-    /** type {number} */
-    this.g = g;
-    /** type {number} */
-    this.b = b;
-    /** type {number} */
-    this.a = a;
+class RGBAColor {
+    constructor(r, g, b, a) {
+        /** type {number} */
+        this.r = r;
+        /** type {number} */
+        this.g = g;
+        /** type {number} */
+        this.b = b;
+        /** type {number} */
+        this.a = a;
+    }
+    rgbaStyle() {
+        return (
+            "rgba(" +
+            ((this.r * 255) >> 0) +
+            "," +
+            ((this.g * 255) >> 0) +
+            "," +
+            ((this.b * 255) >> 0) +
+            "," +
+            this.a.toFixed(2) +
+            ")"
+        );
+    }
+    /**
+     * @param other {RGBAColor}
+     * @return {boolean}
+     */
+    equals(other) {
+        return this.r === other.r && this.g === other.g && this.b === other.b && this.a === other.a;
+    }
+    copy() {
+        return new RGBAColor(this.r, this.g, this.b, this.a);
+    }
+    copyFrom(source) {
+        this.r = source.r;
+        this.g = source.g;
+        this.b = source.b;
+        this.a = source.a;
+    }
+    add(other) {
+        this.r += other.r;
+        this.g += other.g;
+        this.b += other.b;
+        this.a += other.a;
+    }
+    multiply(s) {
+        this.r *= s;
+        this.g *= s;
+        this.b *= s;
+        this.a *= s;
+    }
 }
-
-RGBAColor.prototype.rgbaStyle = function () {
-    return (
-        "rgba(" +
-        ((this.r * 255) >> 0) +
-        "," +
-        ((this.g * 255) >> 0) +
-        "," +
-        ((this.b * 255) >> 0) +
-        "," +
-        this.a.toFixed(2) +
-        ")"
-    );
-};
-
-/**
- * @param other {RGBAColor}
- * @return {boolean}
- */
-RGBAColor.prototype.equals = function (other) {
-    return this.r === other.r && this.g === other.g && this.b === other.b && this.a === other.a;
-};
-
-RGBAColor.prototype.copy = function () {
-    return new RGBAColor(this.r, this.g, this.b, this.a);
-};
-
-RGBAColor.prototype.copyFrom = function (source) {
-    this.r = source.r;
-    this.g = source.g;
-    this.b = source.b;
-    this.a = source.a;
-};
-
-RGBAColor.prototype.add = function (other) {
-    this.r += other.r;
-    this.g += other.g;
-    this.b += other.b;
-    this.a += other.a;
-};
-
-RGBAColor.prototype.multiply = function (s) {
-    this.r *= s;
-    this.g *= s;
-    this.b *= s;
-    this.a *= s;
-};
 
 /**
  * @type {RGBAColor}
