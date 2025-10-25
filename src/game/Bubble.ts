d6128ba298cd5ee9282d23bf5d5423b5969a3d80
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-25 23:08:01 +0700

    Update Bubble.ts

--- src/game/Bubble.ts
+++ src/game/Bubble.ts
@@ -1,13 +1,16 @@
 import GameObject from "@/visual/GameObject";
 
 class Bubble extends GameObject {
+    popped: boolean;
+    withoutShadow: boolean;
+
     constructor() {
         super();
         this.popped = false;
         this.withoutShadow = false;
     }
 
-    draw() {
+    override draw() {
         if (this.withoutShadow) {
             // only do transformations and draw children
             this.preDraw();
