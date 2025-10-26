0514c263e99a55bebbb1ab9a4053850835f5edcc
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-26 18:07:23 +0700

    Refactor RGBAColor to use static class properties
    Moved constant color instances and styles to static class properties within RGBAColor. Added JSDoc annotations for improved type safety and clarity.

--- src/core/RGBAColor.js
+++ src/core/RGBAColor.js
@@ -7,97 +7,119 @@
  * @param a {number} alpha
  */
 class RGBAColor {
+    /**
+     * @param {number} r
+     * @param {number} g
+     * @param {number} b
+     * @param {number} a
+     */
     constructor(r, g, b, a) {
-        /** type {number} */
+        /** @type {number} */
         this.r = r;
-        /** type {number} */
+        /** @type {number} */
         this.g = g;
-        /** type {number} */
+        /** @type {number} */
         this.b = b;
-        /** type {number} */
+        /** @type {number} */
         this.a = a;
     }
+
     rgbaStyle() {
         return `rgba(${(this.r * 255) >> 0},${(this.g * 255) >> 0},${(this.b * 255) >> 0},${this.a.toFixed(2)})`;
     }
+
     /**
-     * @param other {RGBAColor}
+     * @param {RGBAColor} other
      * @return {boolean}
      */
     equals(other) {
+        typeVerify(other, RGBAColor);
         return this.r === other.r && this.g === other.g && this.b === other.b && this.a === other.a;
     }
+
     copy() {
         return new RGBAColor(this.r, this.g, this.b, this.a);
     }
+
+    /**
+     * @param {{ r: number; g: number; b: number; a: number; }} source
+     */
     copyFrom(source) {
         this.r = source.r;
         this.g = source.g;
         this.b = source.b;
         this.a = source.a;
     }
+
+    /**
+     * @param {{ r: number; g: number; b: number; a: number; }} other
+     */
     add(other) {
         this.r += other.r;
         this.g += other.g;
         this.b += other.b;
         this.a += other.a;
     }
+
+    /**
+     * @param {number} s
+     */
     multiply(s) {
         this.r *= s;
         this.g *= s;
         this.b *= s;
         this.a *= s;
     }
+
+    /**
+     * @type {RGBAColor}
+     * @const
+     */
+    static transparent = new RGBAColor(0, 0, 0, 0);
+
+    /**
+     * @type {RGBAColor}
+     * @const
+     */
+    static solidOpaque = new RGBAColor(1, 1, 1, 1);
+
+    /**
+     * @type {RGBAColor}
+     * @const
+     */
+    static red = new RGBAColor(1, 0, 0, 1);
+
+    /**
+     * @type {RGBAColor}
+     * @const
+     */
+    static blue = new RGBAColor(0, 0, 1, 1);
+
+    /**
+     * @type {RGBAColor}
+     * @const
+     */
+    static green = new RGBAColor(0, 1, 0, 1);
+
+    /**
+     * @type {RGBAColor}
+     * @const
+     */
+    static black = new RGBAColor(0, 0, 0, 1);
+
+    /**
+     * @type {RGBAColor}
+     * @const
+     */
+    static white = RGBAColor.solidOpaque;
+
+    /**
+     * @enum {string}
+     */
+    static styles = {
+        SOLID_OPAQUE: "rgba(255,255,255,1)",
+        TRANSPARENT: "rgba(0,0,0,0)",
+    };
 }
-
-/**
- * @type {RGBAColor}
- * @const
- */
-RGBAColor.transparent = new RGBAColor(0, 0, 0, 0);
-
-/**
- * @type {RGBAColor}
- * @const
- */
-RGBAColor.solidOpaque = new RGBAColor(1, 1, 1, 1);
-
-/**
- * @type {RGBAColor}
- * @const
- */
-RGBAColor.red = new RGBAColor(1, 0, 0, 1);
-
-/**
- * @type {RGBAColor}
- * @const
- */
-RGBAColor.blue = new RGBAColor(0, 0, 1, 1);
-
-/**
- * @type {RGBAColor}
- * @const
- */
-RGBAColor.green = new RGBAColor(0, 1, 0, 1);
-
-/**
- * @type {RGBAColor}
- * @const
- */
-RGBAColor.black = new RGBAColor(0, 0, 0, 1);
-
-/**
- * @type {RGBAColor}
- * @const
- */
-RGBAColor.white = RGBAColor.solidOpaque;
-
-/**
- * @enum {string}
- */
-RGBAColor.styles = {
-    SOLID_OPAQUE: "rgba(255,255,255,1)",
-    TRANSPARENT: "rgba(0,0,0,0)",
-};
 
 export default RGBAColor;
