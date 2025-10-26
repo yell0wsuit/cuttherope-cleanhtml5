71c6e687b2c65abcf1eac5f222b83b49885a608e
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-26 23:21:20 +0700

    Add JSDoc type annotations to Action classes
    Added JSDoc comments to constructors and static methods in ActionData and Action classes to improve type clarity and developer experience.

--- src/visual/Action.js
+++ src/visual/Action.js
@@ -1,4 +1,9 @@
 class ActionData {
+    /**
+     * @param {string} name
+     * @param {number} param
+     * @param {number} subParam
+     */
     constructor(name, param, subParam) {
         this.actionName = name;
         this.actionParam = param;
@@ -7,10 +12,20 @@ class ActionData {
 }
 
 class Action {
+    /**
+     * @param {import('@/visual/GameObject').default | import('@/visual/Animation').default} target
+     * @param {ActionData} data
+     */
     constructor(target, data) {
         this.actionTarget = target;
         this.data = data;
     }
+    /**
+     * @param {import("@/visual/GameObject").default | import("@/visual/Animation").default} target
+     * @param {string} actionName
+     * @param {number} param
+     * @param {number} subParam
+     */
     static create(target, actionName, param, subParam) {
         const data = new ActionData(actionName, param, subParam);
         return new Action(target, data);
