7ff75a9804a06be492383c788a0123ead2a06a23
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-26 17:24:38 +0700

    Update Quad2D.js

--- src/core/Quad2D.js
+++ src/core/Quad2D.js
@@ -7,6 +7,13 @@
  * @param h {number} height
  */
 class Quad2D {
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @param {number} w
+     * @param {number} h
+     */
+
     constructor(x, y, w, h) {
         const rightX = x + w,
             bottomY = y + h;
