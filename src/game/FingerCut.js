a3f8f92891890b70a49f495809c455bd71c138a8
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-27 17:46:39 +0700

    Update FingerCut.js

--- src/game/FingerCut.js
+++ src/game/FingerCut.js
@@ -1,12 +1,26 @@
-/**
- * @constructor
- * @param {Vector} start
- * @param {Vector} end
- * @param {number} startSize
- * @param {number} endSize
- * @param {RGBAColor} color
- */
 class FingerCut {
+    /** @type {Vector} */
+    start;
+
+    /** @type {Vector} */
+    end;
+
+    /** @type {number} */
+    startSize;
+
+    /** @type {number} */
+    endSize;
+
+    /** @type {RGBAColor} */
+    color;
+
+    /**
+     * @param {Vector} start
+     * @param {Vector} end
+     * @param {number} startSize
+     * @param {number} endSize
+     * @param {RGBAColor} color
+     */
     constructor(start, end, startSize, endSize, color) {
         this.start = start;
         this.end = end;
