13b83c7bb7a2f5218af25d9b8626ecb0db4422c1
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-27 17:42:51 +0700

    Update Drawing.js

--- src/game/Drawing.js
+++ src/game/Drawing.js
@@ -3,6 +3,25 @@ import ResourceId from "@/resources/ResourceId";
 import PubSub from "@/utils/PubSub";
 
 class Drawing extends GameObject {
+    /**
+     * @type {boolean}
+     */
+    ingame;
+
+    /**
+     * @type {number}
+     */
+    drawingIndex;
+
+    /**
+     * @type {boolean}
+     */
+    passTransformationsToChilds;
+
+    /**
+     * @param {number} hiddenId
+     * @param {number} drawingIndex
+     */
     constructor(hiddenId, drawingIndex) {
         super();
         this.initTextureWithId(ResourceId.IMG_DRAWING_HIDDEN);
