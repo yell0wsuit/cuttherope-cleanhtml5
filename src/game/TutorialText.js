8f4c1ac73295ba8a65178454b290722689b3cb79
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-27 20:56:48 +0700

    Update TutorialText.js

--- src/game/TutorialText.js
+++ src/game/TutorialText.js
@@ -1,6 +1,11 @@
 import TextImage from "@/visual/TextImage";
 
 class TutorialText extends TextImage {
+    /**
+     * @type {number}
+     */
+    special;
+
     constructor() {
         super();
         this.special = 0;
