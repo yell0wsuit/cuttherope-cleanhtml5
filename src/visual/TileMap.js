faf965e41230646185275ec8870e96a0924ff60f
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-28 22:43:50 +0700

    Add JSDoc type annotations to TileMap.js
    Added detailed JSDoc comments and type annotations to constructors, methods, and properties in TileMap.js for improved code clarity and editor support. Also performed minor code style cleanups for variable declarations and block structure.

--- src/visual/TileMap.js
+++ src/visual/TileMap.js
@@ -13,6 +13,10 @@ import Vector from "@/core/Vector";
  * @param {number} quadIndex
  */
 class TileEntry {
+    /**
+     * @param {number} drawerIndex
+     * @param {number} quadIndex
+     */
     constructor(drawerIndex, quadIndex) {
         this.drawerIndex = drawerIndex;
         this.quad = quadIndex;
@@ -20,6 +24,10 @@ class TileEntry {
 }
 
 class TileMap extends BaseElement {
+    /**
+     * @param {number} rows
+     * @param {number} columns
+     */
     constructor(rows, columns) {
         super();
 
@@ -31,11 +39,24 @@ class TileMap extends BaseElement {
 
         this.parallaxRatio = 1;
 
+        /**
+         * @type {ImageMultiDrawer[]}
+         */
         this.drawers = [];
+        /**
+         * @type {TileEntry[]}
+         */
         this.tiles = [];
 
+        /**
+         * @type {number[][]}
+         */
         this.matrix = [];
+
         for (let i = 0; i < columns; i++) {
+            /**
+             * @type {number[]}
+             */
             const column = (this.matrix[i] = []);
             for (let k = 0; k < rows; k++) {
                 column[k] = Constants.UNDEFINED;
@@ -48,8 +69,19 @@ class TileMap extends BaseElement {
         this.verticalRandom = false;
         this.restoreTileTransparency = true;
         this.randomSeed = MathHelper.randomRange(1000, 2000);
+
+        this.tileWidth = 0;
+        this.tileHeight = 0;
+        this.tileMapWidth = 0;
+        this.tileMapHeight = 0;
+        this.maxColsOnScreen = 0;
+        this.maxRowsOnScreen = 0;
     }
 
+    /**
+     * @param {Texture2D} texture
+     * @param {number} quadIndex
+     */
     addTile(texture, quadIndex) {
         if (quadIndex === Constants.UNDEFINED) {
             this.tileWidth = texture.imageWidth;
@@ -112,6 +144,9 @@ class TileMap extends BaseElement {
         }
     }
 
+    /**
+     * @param {number} ratio
+     */
     setParallaxRation(ratio) {
         this.parallaxRatio = ratio;
     }
@@ -124,6 +159,9 @@ class TileMap extends BaseElement {
         this.updateVars();
     }
 
+    /**
+     * @param {number} repeatType
+     */
     setRepeatVertically(repeatType) {
         this.repeatedVertically = repeatType;
         this.updateVars();
@@ -134,15 +172,11 @@ class TileMap extends BaseElement {
      * @param {Vector} pos
      */
     updateWithCameraPos(pos) {
-        const mx = Math.round(pos.x / this.parallaxRatio),
-            my = Math.round(pos.y / this.parallaxRatio);
-        let tileMapStartX = this.x,
-            tileMapStartY = this.y,
-            a,
-            i,
-            len,
-            v,
-            y;
+        const mx = Math.round(pos.x / this.parallaxRatio);
+        const my = Math.round(pos.y / this.parallaxRatio);
+        let tileMapStartX = this.x;
+        let tileMapStartY = this.y;
+        let a, i, len, v;
 
         if (this.repeatedVertically !== TileMap.RepeatType.NONE) {
             const ys = tileMapStartY - my;
@@ -196,14 +230,16 @@ class TileMap extends BaseElement {
             Math.max(0, cameraInTilemap.y)
         );
 
-        //noinspection JSSuspiciousNameCombination
         const startPos = new Vector(
             ~~(~~checkPoint.x / this.tileWidth),
             ~~(~~checkPoint.y / this.tileHeight)
         );
 
-        const highestQuadY = tileMapStartY + startPos.y * this.tileHeight,
-            currentQuadPos = new Vector(tileMapStartX + startPos.x * this.tileWidth, highestQuadY);
+        const highestQuadY = tileMapStartY + startPos.y * this.tileHeight;
+        const currentQuadPos = new Vector(
+            tileMapStartX + startPos.x * this.tileWidth,
+            highestQuadY
+        );
 
         // reset the number of quads to draw
         for (i = 0, len = this.drawers.length; i < len; i++) {
@@ -247,11 +283,11 @@ class TileMap extends BaseElement {
                     resScreen.h
                 );
 
-                let ri = Math.round(i),
-                    rj = Math.round(j);
+                let ri = Math.round(i);
+                let rj = Math.round(j);
 
                 if (this.repeatedVertically === TileMap.RepeatType.EDGES) {
-                    if (currentQuadPos.y < y) {
+                    if (currentQuadPos.y < this.y) {
                         rj = 0;
                     } else if (currentQuadPos.y >= this.y + this.tileMapHeight) {
                         rj = this.rows - 1;
@@ -284,6 +320,9 @@ class TileMap extends BaseElement {
                     rj = rj % this.rows;
                 }
 
+                /**
+                 * @type {number}
+                 */
                 const tile = this.matrix[ri][rj];
                 if (tile >= 0) {
                     const entry = this.tiles[tile],
