9770ef18ffee4b5677cb7c5e865e2a47d2327411
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-30 19:46:39 +0700

    Update teleport.js

--- src/gameScene/sceneUpdate/teleport.js
+++ src/gameScene/sceneUpdate/teleport.js
@@ -4,39 +4,39 @@ import Radians from "@/utils/Radians";
 
 const GameSceneTeleport = (Base) =>
     class extends Base {
-    teleport() {
-        if (!this.targetSock) {
-            return;
+        teleport() {
+            if (!this.targetSock) {
+                return;
+            }
+
+            this.targetSock.light.playTimeline(0);
+            this.targetSock.light.visible = true;
+
+            const off = new Vector(0, resolution.SOCK_TELEPORT_Y);
+            off.rotate(Radians.fromDegrees(this.targetSock.rotation));
+
+            this.star.pos.x = this.targetSock.x;
+            this.star.pos.y = this.targetSock.y;
+            this.star.pos.add(off);
+
+            this.star.prevPos.copyFrom(this.star.pos);
+
+            this.star.v.x = 0;
+            this.star.v.y = -1;
+            this.star.v.rotate(Radians.fromDegrees(this.targetSock.rotation));
+            this.star.v.multiply(this.savedSockSpeed);
+
+            this.star.posDelta.copyFrom(this.star.v);
+            this.star.posDelta.divide(60);
+            this.star.prevPos.copyFrom(this.star.pos);
+            this.star.prevPos.subtract(this.star.posDelta);
+            /**
+             * @type {Sock | null}
+             */
+            this.targetSock = null;
+
+            //Achievements.increment(AchievementId.MAGICIAN);
         }
-
-        this.targetSock.light.playTimeline(0);
-        this.targetSock.light.visible = true;
-
-        const off = new Vector(0, resolution.SOCK_TELEPORT_Y);
-        off.rotate(Radians.fromDegrees(this.targetSock.rotation));
-
-        this.star.pos.x = this.targetSock.x;
-        this.star.pos.y = this.targetSock.y;
-        this.star.pos.add(off);
-
-        this.star.prevPos.copyFrom(this.star.pos);
-
-        this.star.v.x = 0;
-        this.star.v.y = -1;
-        this.star.v.rotate(Radians.fromDegrees(this.targetSock.rotation));
-        this.star.v.multiply(this.savedSockSpeed);
-
-        this.star.posDelta.copyFrom(this.star.v);
-        this.star.posDelta.divide(60);
-        this.star.prevPos.copyFrom(this.star.pos);
-        this.star.prevPos.subtract(this.star.posDelta);
-        /**
-         * @type {Sock | null}
-         */
-        this.targetSock = null;
-
-        //Achievements.increment(AchievementId.MAGICIAN);
-    }
     };
 
 export default GameSceneTeleport;
