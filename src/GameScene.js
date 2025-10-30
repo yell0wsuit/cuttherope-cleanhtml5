4a405b9c2c52ac6aee6636bcae1d08537d0931cd
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-30 14:51:51 +0700

    Refactor GameScene to use delegate pattern
    Replaces mixin-based composition in GameScene with a delegate registration system using proxies, improving modularity and method resolution. Updates GameSceneUpdate and related imports to use class-based delegates, allowing for more flexible and maintainable scene extension.

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,26 +1,99 @@
 import BaseElement from "@/visual/BaseElement";
-import { GameSceneInit } from "@/gameScene/init";
-import { GameSceneLoaders } from "@/gameScene/loaders";
-import { GameSceneCharacter } from "@/gameScene/character";
-import { GameSceneUpdate } from "@/gameScene/update";
-import { GameSceneTouch } from "@/gameScene/touch";
+import GameSceneInit from "@/gameScene/init";
+import GameSceneLoaders from "@/gameScene/loaders";
+import GameSceneCharacter from "@/gameScene/character";
+import GameSceneUpdate from "@/gameScene/update";
+import GameSceneTouch from "@/gameScene/touch";
 
 class GameScene extends BaseElement {
     constructor() {
         super();
-        // Call the init method from the mixin after BaseElement constructor
+
+        /** @type {Array<object>} */
+        this._delegateInstances = [];
+        /** @type {Record<string, object>} */
+        this._delegateMethodMap = Object.create(null);
+        /** @type {WeakMap<object, object>} */
+        this._delegateTargetMap = new WeakMap();
+
+        this._registerDelegate("init", new GameSceneInit(this));
+        this._registerDelegate("loaders", new GameSceneLoaders(this));
+        this._registerDelegate("character", new GameSceneCharacter(this));
+        this._registerDelegate("updateCoordinator", new GameSceneUpdate(this));
+        this._registerDelegate("touchHandlers", new GameSceneTouch(this));
+
         this.init();
     }
-}
 
-// Apply mixins to the class
-Object.assign(
-    GameScene.prototype,
-    GameSceneInit,
-    GameSceneLoaders,
-    GameSceneCharacter,
-    GameSceneUpdate,
-    GameSceneTouch
-);
+    _registerDelegate(name, delegate) {
+        const scene = this;
+        const handler = {
+            get(target, prop, receiver) {
+                if (prop === "scene") {
+                    return scene;
+                }
+                if (prop in target) {
+                    const value = Reflect.get(target, prop, receiver);
+                    return typeof value === "function" ? value.bind(receiver) : value;
+                }
+
+                const owner = scene._delegateMethodMap[prop];
+                if (owner) {
+                    const ownerTarget = scene._delegateTargetMap.get(owner);
+                    if (ownerTarget) {
+                        const value = Reflect.get(ownerTarget, prop, owner);
+                        return typeof value === "function" ? value.bind(owner) : value;
+                    }
+                }
+
+                const sceneValue = Reflect.get(scene, prop);
+                return typeof sceneValue === "function" ? sceneValue.bind(scene) : sceneValue;
+            },
+            set(target, prop, value, receiver) {
+                if (prop === "scene") {
+                    return false;
+                }
+
+                if (Object.prototype.hasOwnProperty.call(target, prop)) {
+                    return Reflect.set(target, prop, value, receiver);
+                }
+
+                scene[prop] = value;
+                return true;
+            },
+            has(target, prop) {
+                return (
+                    prop in target ||
+                    prop in scene ||
+                    Object.prototype.hasOwnProperty.call(scene._delegateMethodMap, prop)
+                );
+            },
+        };
+
+        const proxiedDelegate = new Proxy(delegate, handler);
+        this._delegateTargetMap.set(proxiedDelegate, delegate);
+
+        if (name && !Object.prototype.hasOwnProperty.call(this, name)) {
+            this[name] = proxiedDelegate;
+        }
+
+        this._delegateInstances.push(proxiedDelegate);
+
+        const proto = Object.getPrototypeOf(delegate);
+        const methodNames = Object.getOwnPropertyNames(proto).filter(
+            (methodName) =>
+                methodName !== "constructor" && typeof delegate[methodName] === "function"
+        );
+
+        for (const methodName of methodNames) {
+            this._delegateMethodMap[methodName] = proxiedDelegate;
+            this[methodName] = (...args) => proxiedDelegate[methodName](...args);
+        }
+    }
+
+    registerDelegate(name, delegate) {
+        this._registerDelegate(name, delegate);
+    }
+}
 
 export default GameScene;
--- src/gameScene/update.js
+++ src/gameScene/update.js
@@ -1,25 +1,35 @@
 import { update } from "./sceneUpdate/main";
-import { GameSceneDraw } from "./sceneUpdate/draw";
-import { GameSceneBubbles } from "./sceneUpdate/bubbles";
-import { GameSceneTeleport } from "./sceneUpdate/teleport";
-import { GameSceneLifecycle } from "./sceneUpdate/lifecycle";
-import { GameSceneRopeManagement } from "./sceneUpdate/ropeManagement";
-import { GameScenePumpUtils } from "./sceneUpdate/pumpUtils";
-import { GameSceneBounceUtils } from "./sceneUpdate/bounceUtils";
-import { GameSceneCut } from "./sceneUpdate/cut";
-import { GameSceneSpiderHandlers } from "./sceneUpdate/spiderHandlers";
-import { GameSceneSelection } from "./sceneUpdate/selection";
+import GameSceneDraw from "./sceneUpdate/draw";
+import GameSceneBubbles from "./sceneUpdate/bubbles";
+import GameSceneTeleport from "./sceneUpdate/teleport";
+import GameSceneLifecycle from "./sceneUpdate/lifecycle";
+import GameSceneRopeManagement from "./sceneUpdate/ropeManagement";
+import GameScenePumpUtils from "./sceneUpdate/pumpUtils";
+import GameSceneBounceUtils from "./sceneUpdate/bounceUtils";
+import GameSceneCut from "./sceneUpdate/cut";
+import GameSceneSpiderHandlers from "./sceneUpdate/spiderHandlers";
+import GameSceneSelection from "./sceneUpdate/selection";
 
-export const GameSceneUpdate = {
-    update,
-    ...GameSceneDraw,
-    ...GameSceneBubbles,
-    ...GameSceneTeleport,
-    ...GameSceneLifecycle,
-    ...GameSceneRopeManagement,
-    ...GameScenePumpUtils,
-    ...GameSceneBounceUtils,
-    ...GameSceneCut,
-    ...GameSceneSpiderHandlers,
-    ...GameSceneSelection,
-};
+class GameSceneUpdate {
+    constructor(scene) {
+        scene.registerDelegate("draw", new GameSceneDraw(scene));
+        scene.registerDelegate("bubbles", new GameSceneBubbles(scene));
+        scene.registerDelegate("teleport", new GameSceneTeleport(scene));
+        scene.registerDelegate("lifecycle", new GameSceneLifecycle(scene));
+        scene.registerDelegate("ropeManagement", new GameSceneRopeManagement(scene));
+        scene.registerDelegate("pumpUtils", new GameScenePumpUtils(scene));
+        scene.registerDelegate("bounceUtils", new GameSceneBounceUtils(scene));
+        scene.registerDelegate("cutHandlers", new GameSceneCut(scene));
+        scene.registerDelegate("spiderHandlers", new GameSceneSpiderHandlers(scene));
+        scene.registerDelegate("selectionHandlers", new GameSceneSelection(scene));
+    }
+
+    /**
+     * @param {number} delta
+     */
+    update(delta) {
+        update.call(this, delta);
+    }
+}
+
+export default GameSceneUpdate;
