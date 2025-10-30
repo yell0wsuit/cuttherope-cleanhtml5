9a4a2b1493724356f46a488958e526afaa99af19
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-10-30 13:23:52 +0700

    Refactor GameSceneBubbles to ES6 class syntax
    Converted GameSceneBubbles from an object to an ES6 class, added JSDoc comments for methods, and added debug console.log statements in isBubbleCapture and handleBubbleTouch. This improves code structure, maintainability, and type clarity.

--- src/gameScene/sceneUpdate/bubbles.js
+++ src/gameScene/sceneUpdate/bubbles.js
@@ -4,7 +4,13 @@ import ResourceId from "@/resources/ResourceId";
 import SoundMgr from "@/game/CTRSoundMgr";
 import resolution from "@/resolution";
 
-export const GameSceneBubbles = {
+class GameSceneBubbles {
+    /**
+     * @param {Bubble} b
+     * @param {GameObject} candy
+     * @param {Bubble | null} candyBubble
+     * @param {AnimationPool} candyBubbleAnimation
+     */
     isBubbleCapture(b, candy, candyBubble, candyBubbleAnimation) {
         const bubbleSize = resolution.BUBBLE_SIZE;
         const bubbleSizeDouble = bubbleSize * 2;
@@ -24,6 +30,8 @@ export const GameSceneBubbles = {
             }
             candyBubbleAnimation.visible = true;
 
+            console.log(candyBubbleAnimation);
+
             SoundMgr.playSound(ResourceId.SND_BUBBLE);
 
             b.popped = true;
@@ -34,24 +42,42 @@ export const GameSceneBubbles = {
             return true;
         }
         return false;
-    },
+    }
+
+    /**
+     * @param {boolean} isLeft
+     */
     popCandyBubble(isLeft) {
         if (this.twoParts !== GameSceneConstants.PartsType.NONE) {
             if (isLeft) {
+                /**
+                 * @type {Bubble | null}
+                 */
                 this.candyBubbleL = null;
                 this.candyBubbleAnimationL.visible = false;
                 this.popBubble(this.candyL.x, this.candyL.y);
             } else {
+                /**
+                 * @type {Bubble | null}
+                 */
                 this.candyBubbleR = null;
                 this.candyBubbleAnimationR.visible = false;
                 this.popBubble(this.candyR.x, this.candyR.y);
             }
         } else {
+            /**
+             * @type {Bubble | null}
+             */
             this.candyBubble = null;
             this.candyBubbleAnimation.visible = false;
             this.popBubble(this.candy.x, this.candy.y);
         }
-    },
+    }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     */
     popBubble(x, y) {
         this.detachCandy();
 
@@ -62,8 +88,15 @@ export const GameSceneBubbles = {
 
         this.bubbleDisappear.playTimeline(0);
         this.aniPool.addChild(this.bubbleDisappear);
-    },
+    }
+
+    /**
+     * @param {ConstrainedPoint} s
+     * @param {number} tx
+     * @param {number} ty
+     */
     handleBubbleTouch(s, tx, ty) {
+        console.log(s);
         if (
             Rectangle.pointInRect(
                 tx + this.camera.pos.x,
@@ -82,5 +115,7 @@ export const GameSceneBubbles = {
             return true;
         }
         return false;
-    },
-};
+    }
+}
+
+export default GameSceneBubbles;
