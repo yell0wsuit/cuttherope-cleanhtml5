37b6cf56e60e8d5232134a26490ce859481d9d6b
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2025-11-07 23:53:00 +0700

    convert gameScene collisionHelpers to ts

--- src/gameScene/sceneUpdate/collisionHelpers.ts
+++ src/gameScene/sceneUpdate/collisionHelpers.ts
@@ -1,30 +1,24 @@
 import Rectangle from "@/core/Rectangle";
 import Vector from "@/core/Vector";
+import type ConstrainedPoint from "@/physics/ConstrainedPoint";
 
-/**
- * @param {ConstrainedPoint} star
- * @param {number} rd
- * @param {number} yImpulse
- * @param {number} delta
- */
-export function applyStarImpulse(star, rd, yImpulse, delta) {
+export const applyStarImpulse = (
+    star: ConstrainedPoint,
+    rd: number,
+    yImpulse: number,
+    delta: number
+) => {
     star.applyImpulse(new Vector(-star.v.x / rd, -star.v.y / rd + yImpulse), delta);
+};
+
+interface HazardWithBounds {
+    t1: Vector;
+    t2: Vector;
+    b1: Vector;
+    b2: Vector;
 }
 
-/**
- * @typedef {Object} HazardWithBounds
- * @property {Vector} t1 - Top-left corner
- * @property {Vector} t2 - Top-right corner
- * @property {Vector} b1 - Bottom-left corner
- * @property {Vector} b2 - Bottom-right corner
- */
-
-/**
- * @param {HazardWithBounds} hazard
- * @param {ConstrainedPoint} star
- * @param {number} radius
- */
-export function isCandyHit(hazard, star, radius) {
+export const isCandyHit = (hazard: HazardWithBounds, star: ConstrainedPoint, radius: number) => {
     const diameter = radius * 2;
     return (
         Rectangle.lineInRect(
@@ -48,4 +42,4 @@ export function isCandyHit(hazard, star, radius) {
             diameter
         )
     );
-}
+};
